24ba6aa304561f9635d17b7071e7d25c6e6fc9a5
Fhuraï <Fhurai@users.noreply.github.com>
2025-03-15 11:43:45 +0100

    Delete v1 directory

--- v1/js/utilities.js
+++
@@ -1,9 +0,0 @@
-// Method to add spinner to HTML element.
-function showSpinner(element){
-	element.innerHTML = "<div id='spinner'></div>";
-}
-
-// Method to delete a spinner in a HTML Element by a string.
-function hideSpinnerText(element, string){
-	element.innerText = string;
-}
